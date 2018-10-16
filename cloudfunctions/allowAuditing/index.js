2cd1065e25f5d50d72501cb1578b6dab1301ae15
lmy98129 <41624140@xs.ustb.edu.cn>
2018-10-17 01:36:20 +0800

    template-message abstract router done

--- cloudfunctions/allowAuditing/index.js
+++ cloudfunctions/allowAuditing/index.js
@@ -6,17 +6,17 @@ cloud.init()
 
 const db = cloud.database();
 const _ = db.command;
-const messageUrl = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token="
+// const messageUrl = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token="
 
-const apiReq = (url, body) => new Promise((resolve, reject) => {
-  request({url, method: "POST", json: true, body}, (err, res, body) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(body);
-    }
-  })
-})
+// const apiReq = (url, body) => new Promise((resolve, reject) => {
+//   request({url, method: "POST", json: true, body}, (err, res, body) => {
+//     if (err) {
+//       reject(err);
+//     } else {
+//       resolve(body);
+//     }
+//   })
+// })
 
 // 云函数入口函数
 exports.main = async (event, context) => {
@@ -40,39 +40,19 @@ exports.main = async (event, context) => {
       }
     })
 
-
-    // await Promise.all(newIdList.map(item => {
-    //   return db.collection("auth").doc(item).update({
+    // while(1) {
+    //   res = await cloud.callFunction({
+    //     name: "notification",
     //     data: {
-    //       status: "authorized"
+    //       $url: "check"
     //     }
-    //   })
-    // }));
+    //   });
+    //   if (res.result.code === 1) {
+    //     break;
+    //   }
+    // }
 
-    // await Promise.all(newOpenidList.map(item => {
-    //   return db.collection("user-notification").add({
-    //     data: {
-    //       userOpenid: item,
-    //       msgId: "W7ZAvQIrVDZJFsXO",
-    //       status: "un-read",
-    //       createTime: db.serverDate()
-    //     }
-    //   })
-    // }));
-
-    while(1) {
-      res = await cloud.callFunction({
-        name: "notification",
-        data: {
-          $url: "check"
-        }
-      });
-      if (res.result.code === 1) {
-        break;
-      }
-    }
-
-    let accessToken = res.result.accessToken;
+    // let accessToken = res.result.accessToken;
 
     let myDate = new Date();
     myDate = new Date(myDate.setHours(myDate.getHours() + 8));
@@ -92,63 +72,74 @@ exports.main = async (event, context) => {
         }
       }
     }
-    
-    await Promise.all(newOpenidList.map(item => {
-      return db.collection("formid").where({
-        _openid: item
-      }).get().then(res => {
-        if (res.data.length === 0 || res.data[0].formidArray.length === 0) return;
-        else {
-          let tmpArray = res.data[0].formidArray;
-          let formid;
-          while(1) {
-            formid = tmpArray.shift();
-            if (Date.parse(myDate) < Date.parse(formid.timeout)) break;
-          }
-          let tmpData = data;
-          tmpData["touser"] = item;
-          tmpData["form_id"] = formid.formid;
-          console.log(res.data[0]._id);
-          return db.collection("formid").doc(res.data[0]._id).update({
-            data: {
-              formidArray: JSON.parse(JSON.stringify(tmpArray))
-            }
-          })
-          .then(res => {
-            console.log(res);
-            console.log(messageUrl+accessToken);
-            console.log(tmpData);
-            return apiReq(messageUrl+accessToken, tmpData)
-          }).then(res => {
-            console.log(res);
-            if (res.errcode !== 0) {
-              return cloud.callFunction({
-                name: "notification",
-                data: {
-                  $url: "check",
-                  isCheckNow: true
-                }
-              })
-            } else {
-              return Promise.resolve("");
-            }
-          })
-          .then(res => {
-            console.log(res);
-            if (res !== "" && res.result.code === 1) {
-              accessToken = res.assessToken;
-              return apiReq(messageUrl+accessToken, tmpData)
-            }
-          })
-          .catch(err => {console.log(err)});
-        }
-      })
-    }))
 
-    return {
-      code: 0,
-      msg: "allowing succeed"
-    }
+    res = await cloud.callFunction({
+      name: "notification",
+      data: {
+        $url: "send",
+        data,
+        openidList: newOpenidList
+      }
+    });
+
+    return res.result;
+    
+    // await Promise.all(newOpenidList.map(item => {
+    //   return db.collection("formid").where({
+    //     _openid: item
+    //   }).get().then(res => {
+    //     if (res.data.length === 0 || res.data[0].formidArray.length === 0) return;
+    //     else {
+    //       let tmpArray = res.data[0].formidArray;
+    //       let formid;
+    //       while(1) {
+    //         formid = tmpArray.shift();
+    //         if (Date.parse(myDate) < Date.parse(formid.timeout)) break;
+    //       }
+    //       let tmpData = data;
+    //       tmpData["touser"] = item;
+    //       tmpData["form_id"] = formid.formid;
+    //       console.log(res.data[0]._id);
+    //       return db.collection("formid").doc(res.data[0]._id).update({
+    //         data: {
+    //           formidArray: JSON.parse(JSON.stringify(tmpArray))
+    //         }
+    //       })
+    //       .then(res => {
+    //         console.log(res);
+    //         console.log(messageUrl+accessToken);
+    //         console.log(tmpData);
+    //         return apiReq(messageUrl+accessToken, tmpData)
+    //       }).then(res => {
+    //         console.log(res);
+    //         if (res.errcode !== 0) {
+    //           return cloud.callFunction({
+    //             name: "notification",
+    //             data: {
+    //               $url: "check",
+    //               isCheckNow: true
+    //             }
+    //           })
+    //         } else {
+    //           return Promise.resolve("");
+    //         }
+    //       })
+    //       .then(res => {
+    //         console.log(res);
+    //         if (res !== "" && res.result.code === 1) {
+    //           accessToken = res.assessToken;
+    //           return apiReq(messageUrl+accessToken, tmpData)
+    //         }
+    //       })
+    //       .catch(err => {console.log(err)});
+    //     }
+    //   })
+    // }))
+
+    // return {
+    //   code: 0,
+    //   msg: "allowing succeed"
+    // }
 
   } catch(error) {
     console.log(error);
--- cloudfunctions/notification/index.js
+++ cloudfunctions/notification/index.js
@@ -8,11 +8,12 @@ cloud.init()
 const db = cloud.database();
 const _ = db.command;
 const accessTokenUrl = "https://api.weixin.qq.com/cgi-bin/token?";
+const messageUrl = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=";
 const grant_type = "client_credential";
 const appid = "wxe9b916dde775d246";
 const appsecret = "2998fbb540dcd5bc47c3cea6738ba77a";
 
-const apiReq = (url) => new Promise((resolve, reject) => {
+const apiReqGet = (url) => new Promise((resolve, reject) => {
   request.get(url, (err, res, body) => {
     if (err) {
       reject(err);
@@ -21,6 +22,86 @@ const apiReq = (url) => new Promise((resolve, reject) => {
     }
   })
 })
+
+const apiReqPost = (url, body) => new Promise((resolve, reject) => {
+  request({url, method: "POST", json: true, body}, (err, res, body) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(body);
+    }
+  })
+})
+
+const check = async () => {
+  try {
+    let msg;
+    let res = await db.collection("notification").where({
+      special: "accessToken"
+    }).get();
+    if (res.data.length === 0) {
+      let reqRes = await apiReqGet(accessTokenUrl+"grant_type="+grant_type+"&appid="+appid+"&secret="+appsecret);
+      reqRes = JSON.parse(reqRes);
+      if (reqRes["access_token"] === undefined && reqRes["errcode"] !== 0) {
+        msg = {
+          code: -2,
+        }
+      } else {
+        msg = {
+          code: 1,
+          accessToken: reqRes["access_token"]
+        }
+        await db.collection("notification").add({
+          data: {
+            special: "accessToken",
+            accessToken: reqRes["access_token"],
+            timeout: db.serverDate({
+              offset: reqRes["expires_in"] * 1000
+            })
+          }
+        });
+      }
+
+      
+    } else {
+      let myDate = new Date();
+      if (Date.parse(new Date(myDate.setHours(myDate.getHours() + 8))) > Date.parse(res.data[0].timeout) || event.isCheckNow) {
+        let reqRes = await apiReqGet(accessTokenUrl+"grant_type="+grant_type+"&appid="+appid+"&secret="+appsecret);
+        reqRes = JSON.parse(reqRes);          
+        if (reqRes["access_token"] === undefined && reqRes["errcode"] !== 0) {
+          msg = {
+            code: -2,
+          }
+        } else {
+          msg = {
+            code: 1,
+            accessToken: reqRes["access_token"]
+          }
+          await db.collection("notification").doc(res.data[0]._id).update({
+            data: {
+              accessToken: reqRes["access_token"],
+              timeout: db.serverDate({
+                offset: reqRes["expires_in"] * 1000
+              })
+            }
+          })
+        }
+      } else {
+        msg = {
+          code: 1,
+          accessToken: res.data[0].accessToken
+        }
+      }
+    }
+    return msg;
+  } catch (error) {
+    msg = {
+      code: -1,
+    }
+    console.log(error);
+    return msg;
+  }
+}
 
 // 云函数入口函数
 exports.main = async (event, context) => {
@@ -62,67 +143,72 @@ exports.main = async (event, context) => {
   });
 
   app.router("check", async (ctx) => {
+    ctx.body = check();
+  })
+
+  app.router("send", async (ctx) => {
+    let result;
     try {
-      let res = await db.collection("notification").where({
-        special: "accessToken"
+      while(1) {
+        result = await check();
+        if (result.code === 1) break;
+      }
+
+    let accessToken = result.accessToken;
+    let data = event.data;
+    let openidList = event.openidList;
+    let myDate = new Date();
+    myDate = new Date(myDate.setHours(myDate.getHours() + 8));
+    console.log(openidList);
+    
+    for (let item of openidList) {
+      console.log("hey", item);
+      let res = await db.collection("formid").where({
+        _openid: item
       }).get();
-      if (res.data.length === 0) {
-        let reqRes = await apiReq(accessTokenUrl+"grant_type="+grant_type+"&appid="+appid+"&secret="+appsecret);
-        reqRes = JSON.parse(reqRes);
-        if (reqRes["access_token"] === undefined && reqRes["errcode"] !== 0) {
-          ctx.body = {
-            code: -2,
-          }
-        } else {
-          ctx.body = {
-            code: 1,
-            accessToken: reqRes["access_token"]
-          }
-          await db.collection("notification").add({
-            data: {
-              special: "accessToken",
-              accessToken: reqRes["access_token"],
-              timeout: db.serverDate({
-                offset: reqRes["expires_in"] * 1000
-              })
-            }
-          });
+      console.log(res);
+      if (res.data.length === 0 || res.data[0].formidArray.length === 0) return;
+      else {
+        let tmpArray = res.data[0].formidArray;
+        let formid;
+        while(tmpArray.length > 0) {
+          formid = tmpArray.shift();
+          if (Date.parse(myDate) < Date.parse(formid.timeout)) break;
+          if (tmpArray.length == 0) return;
         }
-      } else {
-        let myDate = new Date();
-        if (Date.parse(new Date(myDate.setHours(myDate.getHours() + 8))) > Date.parse(res.data[0].timeout) || event.isCheckNow) {
-          let reqRes = await apiReq(accessTokenUrl+"grant_type="+grant_type+"&appid="+appid+"&secret="+appsecret);
-          reqRes = JSON.parse(reqRes);          
-          if (reqRes["access_token"] === undefined && reqRes["errcode"] !== 0) {
-            ctx.body = {
-              code: -2,
-            }
-          } else {
-            ctx.body = {
-              code: 1,
-              accessToken: reqRes["access_token"]
-            }
-            await db.collection("notification").doc(res.data[0]._id).update({
-              data: {
-                accessToken: reqRes["access_token"],
-                timeout: db.serverDate({
-                  offset: reqRes["expires_in"] * 1000
-                })
-              }
-            })
+        let tmpData = data;
+        tmpData["touser"] = item;
+        tmpData["form_id"] = formid.formid;
+        console.log(res.data[0]._id);
+        await db.collection("formid").doc(res.data[0]._id).update({
+          data: {
+            formidArray: JSON.parse(JSON.stringify(tmpArray))
           }
-        } else {
-          ctx.body = {
-            code: 1,
-            accessToken: res.data[0].accessToken
-          }
+        });
+        console.log(tmpData);
+        while(1) {
+          res = await apiReqPost(messageUrl+accessToken, tmpData);
+          console.log(res);
+          if (res.errcode !== 0) {
+            res = await check();
+            accessToken = res.accessToken;
+          } else break;
         }
       }
+    }
+
+    ctx.body = {
+      code: 0,
+      msg: "send succeed"
+    }
+
     } catch (error) {
+      console.log(error);
       ctx.body = {
         code: -1,
+        msg: "send failed",
+        err: error
       }
-      console.log(error);
     }
   })
 
